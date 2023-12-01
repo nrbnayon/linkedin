378626c4358892584e931db96307cf27a81f23d5
nrbnayon <nrbnayon@gmail.com>
2023-12-01 22:20:06 +0600

    Script

---
+++ script.js
@@ -0,0 +1,29 @@
+let profileMenu = document.getElementById("profileMenu");
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");  
+}
+
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+
+}
+
+if(localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else {
+    localStorage.setItem("theme", "light");
+}
