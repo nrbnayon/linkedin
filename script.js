075393f649d2b4b432fe618f0586f0bcaf89e46c
nrbnayon <nrbnayon@gmail.com>
2023-12-05 02:16:07 +0600

    Completed Responsive CSS

--- script.js
+++ script.js
@@ -3,27 +3,40 @@ function toggleMenu(){
     profileMenu.classList.toggle("open-menu");  
 }
 
-darkBtn.onclick = function() {
-    darkBtn.classList.toggle("dark-btn-on");
-    document.body.classList.toggle("dark-theme");
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
 
-    if(localStorage.getItem("theme") == "light") {
-        localStorage.setItem("theme", "dark");
-    }
-    else {
-        localStorage.setItem("theme", "light");
-    }
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
 
+    if(sideActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show less <b>-</b>"
+    }else {
+        moreLink.innerHTML = "Show more <b>+</b>"
+}
 }
 
-if(localStorage.getItem("theme") == "light") {
-    darkBtn.classList.remove("dark-btn-on");
-    document.body.classList.remove("dark-theme");
-}
-else if(localStorage.getItem("theme") == "dark") {
-    darkBtn.classList.add("dark-btn-on");
-    document.body.classList.add("dark-theme");
-}
-else {
-    localStorage.setItem("theme", "light");
-}
+// darkBtn.onclick = function() {
+//     darkBtn.classList.toggle("dark-btn-on");
+//     document.body.classList.toggle("dark-theme");
+
+//     if(localStorage.getItem("theme") == "light") {
+//         localStorage.setItem("theme", "dark");
+//     }
+//     else {
+//         localStorage.setItem("theme", "light");
+//     }
+
+// }
+
+// if(localStorage.getItem("theme") == "light") {
+//     darkBtn.classList.remove("dark-btn-on");
+//     document.body.classList.remove("dark-theme");
+// }
+// else if(localStorage.getItem("theme") == "dark") {
+//     darkBtn.classList.add("dark-btn-on");
+//     document.body.classList.add("dark-theme");
+// }
+// else {
+//     localStorage.setItem("theme", "light");
+// }
